82ba600272daa5041666625737c760112b4ed0ee
Lakshmi <lakshmikshetty21@gmail.com>
2021-02-18 13:18:36 +0530

    Asian guy time zone

--- Automation67/src/test/java/Cucumber/Automation/App.java
+++ Automation67/src/test/java/Cucumber/Automation/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        System.out.println("Welcome to accionlabs");
+        System.out.println("Accionlabs");
     }
 }
